0e8a008f88f1bf91e72f46048b45bf750c9c8f1f
Varun Kumar Madupu <varunkumarmadupu@Varuns-MacBook-Air.local>
2019-10-21 15:58:31 +0530

    Sign in page

---
+++ lib/authentication/sign_in.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class SignInView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("Time Tracker"),
+          centerTitle: true,
+        ),  
+        body: _buildScreen(),
+      ),
+    );
+  }
+
+  Container _buildScreen() {
+    return Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Sign In",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 32.0,
+                fontWeight: FontWeight.w600
+              ),
+            ),
+            SizedBox(
+              height: 8.0,
+            ),
+            RaisedButton(
+              onPressed: (){},
+            )
+          ],
+        ),
+      );
+  }
+}
