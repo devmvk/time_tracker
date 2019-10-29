e4fb2f5f14cde518d01cb8f2006feabf661f9408
Varun Kumar Madupu <developer.varunkumar@gmail.com>
2019-10-29 15:50:12 +0530

    validator class

--- lib/authentication/email_sign_in_form.dart
+++ lib/authentication/email_sign_in_form.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:time_tracker/authentication/form_submit_button.dart';
+import 'package:time_tracker/common/validators.dart';
 import 'package:time_tracker/services/auth.dart';
 
 
 enum EmailFormType{ signIn, register}
 
-class EmailSignInForm extends StatefulWidget {
+class EmailSignInForm extends StatefulWidget with EmailAndPasswordValidator {
 
   final AuthBase auth;
 
@@ -19,12 +20,20 @@ class _EmailSignInFormState extends State<EmailSignInForm> {
   
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final FocusNode _emailFocusNode = FocusNode();
+  final FocusNode _passwordFocusNode = FocusNode();
+
   EmailFormType _formType = EmailFormType.signIn;
 
   String get _email => _emailController.text;
   String get _password => _passwordController.text;
 
+  bool _isSubmitted = false;
+
   void _submit() async{
+    setState(() {
+      _isSubmitted = true;
+    });
     try{
       if(_formType == EmailFormType.signIn){
         await widget.auth.emailAndPasswordSignIn(_email, _password);
@@ -37,8 +46,13 @@ class _EmailSignInFormState extends State<EmailSignInForm> {
     Navigator.of(context).pop();
   }
 
+  void _focusOnPasswordField(){
+    FocusScope.of(context).requestFocus(_passwordFocusNode);
+  }
+
   void _toggleFormType(){
     setState(() {
+      _isSubmitted = false;
       _formType = _formType == EmailFormType.signIn ? EmailFormType.register : EmailFormType.signIn;
     });
     _emailController.clear();
@@ -50,21 +64,9 @@ class _EmailSignInFormState extends State<EmailSignInForm> {
     String _secondaryText = _formType == EmailFormType.signIn ? "Need an Account? Register." : "Have an Account? Sign In";
 
     return [
-      TextField(
-        controller: _emailController,
-        decoration: InputDecoration(
-          labelText: 'Email',
-          hintText: 'test@test.com'
-        ),
-      ),
+      _buildEmailField(),
       SizedBox(height: 8.0,),
-      TextField(
-        controller: _passwordController,
-        decoration: InputDecoration(
-          labelText: 'Password'
-        ),
-        obscureText: true,
-      ),
+      _buildPasswordField(),
       SizedBox(height: 8.0,),
       FormSignInButton(
         text: _primaryText,
@@ -76,6 +78,38 @@ class _EmailSignInFormState extends State<EmailSignInForm> {
         onPressed: _toggleFormType,
       )
     ];
+  }
+
+  TextField _buildPasswordField() {
+    bool _showPasswordErrorText = _isSubmitted && !widget.passwordValidator.isValid(_email);
+    return TextField(
+      controller: _passwordController,
+      decoration: InputDecoration(
+        labelText: 'Password',
+        errorText: _showPasswordErrorText ? widget.passwordErrorText : null
+      ),
+      obscureText: true,
+      textInputAction: TextInputAction.done,
+      focusNode: _passwordFocusNode,
+      onEditingComplete: _submit,
+    );
+  }
+
+  TextField _buildEmailField() {
+    bool _showEmailErrorText = _isSubmitted && !widget.emailValidator.isValid(_email);
+
+    return TextField(
+      controller: _emailController,
+      decoration: InputDecoration(
+        labelText: 'Email',
+        hintText: 'test@test.com',
+        errorText: _showEmailErrorText ? widget.emailErrorText : null
+      ),
+      keyboardType: TextInputType.emailAddress,
+      textInputAction: TextInputAction.next,
+      focusNode: _emailFocusNode,
+      onEditingComplete: _focusOnPasswordField,
+    );
   }
 
   @override
---
+++ lib/common/validators.dart
@@ -0,0 +1,15 @@
+abstract class StringValidator{
+  bool isValid(String text);
+}
+
+class NonEmptyStringValidator extends StringValidator{
+
+  @override isValid(String text) => text.isNotEmpty;
+}
+
+class EmailAndPasswordValidator {
+  final StringValidator emailValidator = NonEmptyStringValidator();
+  final StringValidator passwordValidator = NonEmptyStringValidator();
+  final String emailErrorText = "Email should not be empty";
+  final String passwordErrorText = "Password should not be empty";
+}
