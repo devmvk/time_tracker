0db6402f1289b7a04772cf2e440536b38840cba0
Varun Kumar Madupu <developer.varunkumar@gmail.com>
2019-11-11 19:51:30 +0530

    redo

--- lib/authentication/email_sign_in_page.dart
+++ lib/authentication/email_sign_in_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:time_tracker/authentication/email_sign_in_form_bloc_base.dart';
 
 class EmailSignIn extends StatelessWidget {
-  
+  // ** ReDo this flow
   @override
   Widget build(BuildContext context) {
     return SafeArea(
