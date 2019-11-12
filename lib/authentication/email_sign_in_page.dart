49cd2cee079ffebc91c57bd5c4ac01452b83393b
Varun Kumar Madupu <developer.varunkumar@gmail.com>
2019-11-12 19:00:27 +0530

    remove comment

--- lib/authentication/email_sign_in_page.dart
+++ lib/authentication/email_sign_in_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:time_tracker/authentication/email_sign_in_form_change_notifier.dart';
 
 class EmailSignIn extends StatelessWidget {
-  // ** ReDo this flow
   @override
   Widget build(BuildContext context) {
     return SafeArea(
