92b536c33d36ef48f247ec71d713f3a5e7e19849
Varun Kumar Madupu <developer.varunkumar@gmail.com>
2019-12-04 22:13:06 +0530

    avatar enhancemens

--- lib/home/account_page.dart
+++ lib/home/account_page.dart
@@ -58,9 +58,20 @@ class AccountPage extends StatelessWidget {
   }
 
   Widget _buildUserInfo(User user) {
-    return Avatar(
-      photoUrl: user.avatarUrl,
-      radius: 50,
+    return Column(
+      children: <Widget>[
+        Avatar(
+          photoUrl: user.avatarUrl,
+          radius: 50,
+        ),
+        SizedBox(height: 8),
+        if (user.displayName != null)
+          Text(
+            user.displayName,
+            style: TextStyle(color: Colors.white),
+          ),
+        SizedBox(height: 8),
+      ],
     );
   }
 }
